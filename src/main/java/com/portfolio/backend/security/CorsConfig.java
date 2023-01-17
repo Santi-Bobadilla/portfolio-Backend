9a81c3a853ceac5fc4f5838dc72dd9c19e4ac022
Santi-Bobadilla <santiagobobadilla@gmail.com>
2023-01-17 19:22:56 -0300

    cors 2

--- src/main/java/com/portfolio/backend/security/CorsConfig.java
+++ src/main/java/com/portfolio/backend/security/CorsConfig.java
@@ -4,9 +4,9 @@
  */
 package com.portfolio.backend.security;
 
-import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -19,27 +19,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@SpringBootApplication
 @EnableWebMvc
 @Configuration
-public class CorsConfig {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CorsConfig.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-                                registry.addMapping("/api/login")
-                                        .allowedOrigins("https://portfolio-argentina-prog-92fa8.firebaseapp.com/")
-                                        .allowedMethods("*")
-                                        .exposedHeaders("*");
-                            
-				registry.addMapping("/api/**")
-                                        .allowedOrigins("*")
-                                        .allowedMethods("*");
-			}
-		};
-	}
+public class CorsConfig implements WebMvcConfigurer{
+        
+        public void addCorsMapping(CorsRegistry registry){
+            registry.addMapping("/**");
+        }
+//	public static void main(String[] args) {
+//		SpringApplication.run(CorsConfig.class, args);
+//	}
+//        
+        
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer(){
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//                                registry.addMapping("/api/login")
+//                                        .allowedOrigins("https://portfolio-argentina-prog-92fa8.firebaseapp.com")
+//                                        .allowedMethods("*")
+//                                        .exposedHeaders("*");
+//                            
+//				registry.addMapping("/api/**")
+//                                        .allowedOrigins("*")
+//                                        .allowedMethods("*");
+//			}
+//		};
+//	}
 
 }
