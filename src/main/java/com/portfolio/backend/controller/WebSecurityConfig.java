88b5c973916e36f8eb962a2fd5ca40f522d54bb8
Santi-Bobadilla <santiagobobadilla@gmail.com>
2023-01-20 17:33:27 -0300

    probando deploy 3

--- src/main/java/com/portfolio/backend/controller/WebSecurityConfig.java
+++ src/main/java/com/portfolio/backend/controller/WebSecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -27,7 +27,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @AllArgsConstructor
-@EnableWebSecurity
+//@EnableWebSecurity
 
 public class WebSecurityConfig {
     @Autowired
@@ -48,14 +48,15 @@ public class WebSecurityConfig {
                 .cors()
                 .and()
                 .csrf().disable()
+//                .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/**", "/login")
+                .requestMatchers("/login")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic()
-                .and()
+//                .httpBasic()
+//                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
