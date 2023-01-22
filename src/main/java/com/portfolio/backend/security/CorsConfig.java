caa0023f27f0e8ed2872ebad2f858908dc51f7b1
Santi-Bobadilla <santiagobobadilla@gmail.com>
2023-01-21 21:40:27 -0300

    cambio / por /api en todo 3

--- src/main/java/com/portfolio/backend/security/CorsConfig.java
+++ src/main/java/com/portfolio/backend/security/CorsConfig.java
@@ -4,7 +4,7 @@
  */
 package com.portfolio.backend.security;
 
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -18,32 +18,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{ 
         
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .exposedHeaders("*");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry
+//                .addMapping("/api/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("*")
+//                .exposedHeaders("*");
+//    }
     
-//	@Bean
-//             
-//	public WebMvcConfigurer corsConfigurer(){
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//                                registry.addMapping("/api/**")
-//                                        .allowedOrigins("https://portfolio-arg-prog-1754b.web.app")
-//                                        .allowedMethods("*")
-//                                        .exposedHeaders("*");
-//                            
-////				registry.addMapping("/api/**")
-////                                        .allowedOrigins("*")
-////                                        .allowedMethods("*")
-////                                        .exposedHeaders("*");
-//			}
-//		};
-//	}
+	@Bean
+             
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+                                registry.addMapping("/api/login")
+                                        .allowedOrigins("https://portfolio-arg-prog-1754b.web.app")
+                                        .allowedMethods("*")
+                                        .exposedHeaders("*");
+                            
+				registry.addMapping("/api/**")
+                                        .allowedOrigins("*")
+                                        .allowedMethods("*")
+                                        .exposedHeaders("*");
+			}
+		};
+	}
 
 }
